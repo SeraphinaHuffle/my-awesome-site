7412eec222ad33d7a2134570a0782574a82cbc46
userName <userEmail>
2026-03-23 11:21:22 +0800

    Initial commit with logo click event

---
+++ script.js
@@ -0,0 +1,6 @@
+const logoElement = document.getElementById('my-logo');
+
+logoElement.addEventListener('click', function () {
+    console.log('Welcome to AIteachMe! Click on the logo to learn more about our mission and values.');
+    alert('Welcome to AIteachMe! Click on the logo to learn more about our mission and values.');
+});
